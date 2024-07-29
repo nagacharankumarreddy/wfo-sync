cc839c97ac59828da374e75a4a47ea8df2723e03
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2024-07-29 10:17:43 +0530

    SafeAreaProvider added around app

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, Button, View } from "react-native";
-import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import HomeScreen from "./components/HomeScreen";
 import SetupScreen from "./components/SetupScreen";
 
@@ -27,30 +29,40 @@ function App() {
 
   if (initialRoute === null) {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
+      <SafeAreaProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        </GestureHandlerRootView>
+      </SafeAreaProvider>
     );
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={initialRoute}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={({ navigation }) => ({
-            headerRight: () => (
-              <Button
-                onPress={() => navigation.navigate("Setup")}
-                title="Setup"
-              />
-            ),
-          })}
-        />
-        <Stack.Screen name="Setup" component={SetupScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName={initialRoute}>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={({ navigation }) => ({
+                headerRight: () => (
+                  <Button
+                    onPress={() => navigation.navigate("Setup")}
+                    title="Setup"
+                  />
+                ),
+              })}
+            />
+            <Stack.Screen name="Setup" component={SetupScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
 
